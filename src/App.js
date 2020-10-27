660c2d97c1355f26201b41d55f3559ee0abb12b8
Daniel Jaworski <daniel.jaworski1999@gmail.com>
2020-10-27 22:50:32 +0100

    Done task + RWD(this was done really fast ;) But working, i'm not used breakpoints from other files)

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import edit from "./assets/edit-img.png";
 import React, { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
-  const [value, setValue] = useState(11.90);
+  const value = 11.90;
   const [shipping, setShipping] = useState(23.80);
   const [price, setPrice] = useState(0);
+  const [submit, setSubmit] = useState(false);
+  const [product, setProduct] = useState(false);
   if (count < 0) {
     setCount(0)
   }
@@ -15,13 +17,18 @@ function App() {
     setPrice(value * count);
     setShipping(value * count > 100 ? 0 : 23.80)
   }
-
+  const handleSend = () => {
+    setSubmit(true);
+  }
+  const handleDelete = () => {
+    setProduct(true);
+  }
   return (
     <div className="container">
       <div className="main-view">
         <div className="upper-view">
           <h1>Shopping Cart</h1>
-          <button className="proceed-button">Proceed to checkout</button>
+          <button className="proceed-button" onClick={handleSend}>Proceed to checkout</button>
         </div>
         <div className="bottom-view">
           <div className="shopping-cart">
@@ -35,9 +42,9 @@ function App() {
                 </ul>
               </div>
               <hr />
-              <div className="product">
+              <div className="product" style={{ display: product ? "none" : "" }}>
                 <div className="product-img">
-                  <img className="pointer" src={ximg} alt="picture_x" />
+                  <img className="pointer" src={ximg} alt="picture_x" onClick={handleDelete} />
                   <img src={headphones} className="headphones-photo" alt="picture_headphones_list" />
                 </div>
                 <div className="product-info">
@@ -57,7 +64,8 @@ function App() {
               <button className="update-button" onClick={handleClickValue}>Update Shopping Cart</button>
             </div>
           </div>
-          <div className="basket-cart">
+          <h1 className="send-message" style={{ display: !submit ? "none" : "block" }}>Your order has been submitted successfully</h1>
+          <div className="basket-cart" style={{ display: submit ? "none" : "block" }}>
             <div className="order">
               <div className="mini-container-in-basket">
                 <span>SHIPPING</span>
@@ -78,7 +86,7 @@ function App() {
                   <span>Grand Total</span>
                   <span className="price">${(price + shipping).toFixed(2)}</span>
                 </div>
-                <button>Proceed to checkout</button>
+                <button onClick={handleSend}>Proceed to checkout</button>
               </div>
             </div>
           </div>
